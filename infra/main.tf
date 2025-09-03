b5dbc7db96e70696612dee4feb9463af9c22c26e
dgracilieri <diego.gracilieri@gmail.com>
2025-09-03 09:43:22 -0500

    creacion action e iac

--- infra/main.tf
+++ infra/main.tf
@@ -8,7 +8,48 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "call_record_bucke
   bucket = aws_s3_bucket.s3_bucket[0].id
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      kms_master_key_id = aws_kms_key.s3_kms_key.arn
+      sse_algorithm     = "aws:kms"
     }
   }
 }
+
+resource "aws_kms_key" "s3_kms_key" {
+  description             = "KMS key for S3 bucket encryption"
+  deletion_window_in_days = 10
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "Allow S3 to use the key"
+        Effect = "Allow"
+        Principal = {
+          Service = "s3.amazonaws.com"
+        }
+        Action = [
+          "kms:GenerateDataKey",
+          "kms:Decrypt"
+        ]
+        Resource = "*"
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = data.aws_caller_identity.current.account_id
+          },
+          ArnLike = {
+            "aws:SourceArn" = "arn:aws:s3:::*"
+          }
+        }
+      }
+    ]
+  })
+}
+
