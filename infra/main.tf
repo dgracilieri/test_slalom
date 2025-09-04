b5299ff9760ce7a643a8837be2c27df45d7cec96
dgracilieri <diego.gracilieri@gmail.com>
2025-09-04 09:42:16 -0500

    creacion action e iac

--- infra/main.tf
+++ infra/main.tf
@@ -91,10 +91,10 @@ resource "aws_s3_bucket_versioning" "bucket_versioning" {
     }
 
 
-resource "aws_s3_bucket_acl" "s3_bucket_acl" {
-  bucket = aws_s3_bucket_logging.s3_bucket.id
-  acl    = "private"
-}
+# resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+#   bucket = aws_s3_bucket_logging.s3_bucket.id
+#   acl    = "private"
+# }
 
 resource "aws_s3_bucket_logging" "s3_bucket_log" {
   bucket = "${var.name_prefix}-my-s3-access-logs-${var.name_postfix}"
