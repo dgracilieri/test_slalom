241014feb32ff533430078e3b04c8e88df2513ee
dgracilieri <diego.gracilieri@gmail.com>
2025-09-04 12:16:18 -0500

    creacion action e iac

--- infra/input.tf
+++ infra/input.tf
@@ -13,3 +13,9 @@ variable "s3_bucket_name" {
 variable "create_bucket" {
   type = bool
 }
+
+variable "destroy_infrastructure" {
+  description = "Set to true to destroy all resources. Do not use in production."
+  type        = bool
+  default     = false
+}
--- infra/tfvars/terraform-dev.tfvars
+++ infra/tfvars/terraform-dev.tfvars
@@ -2,3 +2,4 @@ name_prefix    = "test-prueba-1"
 name_postfix   = "dev"
 s3_bucket_name = "test-s3-github-action"
 create_bucket  = true
+destroy_infrastructure = false
